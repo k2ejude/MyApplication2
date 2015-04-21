0950ee8b86f80c7d6d860e2f9922619046618ce8
念祖 <k2ejude@gmail.com>
2015-04-21 20:17:05 +0800

    test

--- app/src/main/java/com/example/jude/myapplication/ProjectActivity.java
+++ app/src/main/java/com/example/jude/myapplication/ProjectActivity.java
@@ -6,15 +6,20 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ProjectActivity extends ActionBarActivity {
 
     private ListView listView;
-    private String[] list = {"鉛筆","原子筆","鋼筆","毛筆","彩色筆"};
-    private ArrayAdapter<String> listAdapter;
+    private ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String,String>>();
+    HashMap<String,String> item1 = new HashMap<String,String>();
+    HashMap<String,String> item2 = new HashMap<String,String>();
+    private SimpleAdapter listAdapter;
     private int a = 0;
 
     @Override
@@ -24,8 +29,27 @@ public class ProjectActivity extends ActionBarActivity {
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
+        item1.put("id","project1");
+        item1.put("name","專案一");
+        item1.put("member","Admin");
+        item1.put("loss","100000");
+        item1.put("priority","5");
+        item1.put("time","25天");
+        list.add(item1);
+
+        item2.put("id","project2");
+        item2.put("name","專案二");
+        item2.put("member","Admin");
+        item2.put("loss","10000");
+        item2.put("priority","2");
+        item2.put("time","50天");
+        list.add(item2);
+
+
         listView = (ListView)findViewById(R.id.listProject);
-        listAdapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, list);
+        listAdapter = new SimpleAdapter(this, list, R.layout.projectlist,
+                new String[]{"id", "name", "member", "loss", "priority", "time"},
+                new int[]{R.id.projectIdText, R.id.projectNameText, R.id.projectMainMemberText, R.id.projectLossText, R.id.projectPriorityText, R.id.projectTimeText});
         listView.setAdapter(listAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
