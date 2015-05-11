f84e0cbb5527ca1ada99f9f3231de45e523cd6f5
念祖 <k2ejude@gmail.com>
2015-05-11 14:12:46 +0800

    android

--- app/src/main/java/com/example/jude/myapplication/ProjectActivity.java
+++ app/src/main/java/com/example/jude/myapplication/ProjectActivity.java
@@ -14,17 +14,14 @@ import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.net.URL;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -91,13 +88,16 @@ public class ProjectActivity extends ActionBarActivity {
             BufferedReader reader = null;
             JSONObject object = null;
             try {
-                HttpClient client = new DefaultHttpClient();
-                URL url = new URL(params[0]);
-                HttpGet get = new HttpGet(String.valueOf(url));
+                DefaultHttpClient client = new DefaultHttpClient();
+                HttpGet get = new HttpGet(params[0]);
                 HttpResponse response = client.execute(get);
-                HttpEntity resEntity = response.getEntity();
-                String a = EntityUtils.toString(resEntity);
-                object = new JSONObject(a);
+                BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                StringBuffer result = new StringBuffer();
+                String line = "";
+                while ((line = rd.readLine()) != null) {
+                    result.append(line);
+                }
+                object = new JSONObject(result.toString());
                 JSONArray array = new JSONArray(object.getString("Message"));
                 for(int i = 0; i < array.length(); i++){
                     HashMap<String, String> item = new HashMap<String, String>();
