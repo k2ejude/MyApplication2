41ace026832463c0a4594d2d2d6221964df48426
念祖 <k2ejude@gmail.com>
2015-05-18 21:36:30 +0800

    Android 修改

---
+++ app/src/main/java/com/example/jude/myapplication/GcmIntentService.java
@@ -0,0 +1,73 @@
+package com.example.jude.myapplication;
+
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+public class GcmIntentService extends IntentService {
+
+    public static final int NOTIFCATION_ID = 1;
+    private NotificationManager mNotificationManager;
+    NotificationCompat.Builder builder;
+    static final String TAG = "GCMDemo";
+
+    public GcmIntentService(String name) {
+        super("413376427270");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Bundle extras = intent.getExtras();
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+        // The getMessageType() intent parameter must be the intent you received
+        // in your BroadcastReceiver.
+        String messageType  = gcm.getMessageType(intent);
+
+        if(!extras.isEmpty()){// has effect of unparcelling Bundle
+             /*
+                         * Filter messages based on message type. Since it is likely that GCM
+                         * will be extended in the future with new message types, just ignore
+                         * any message types you're not interested in, or that you don't
+                         * recognize.
+                         */
+            if(GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR.equals(messageType )) {
+                sendNotification("Send error: " + extras.toString());
+            } else if(GoogleCloudMessaging.MESSAGE_TYPE_DELETED.equals(messageType)){
+                sendNotification("Deleted messages on server: " + extras.toString());
+            } else if(GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)){
+                for(int i=0; i<5; i++){
+                    Log.i(TAG, "Working... " + (i + 1) + "/5 @ " + SystemClock.elapsedRealtime());
+                    try{
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e){}
+                }
+                Log.i(TAG, "Completed work @ " + SystemClock.elapsedRealtime());
+                sendNotification("Received: " + extras.getString("Message").toString());
+                Log.i(TAG, "Received: "+ extras.getString("Message").toString());
+            }else if(GoogleCloudMessaging.MESSAGE_TYPE_SEND_EVENT.equals(messageType)){
+                Log.d("Test","tre");
+            }
+        }
+        GcmBroadcastReceiver.completeWakefulIntent(intent);
+    }
+
+    private void sendNotification(String msg){
+        mNotificationManager = (NotificationManager)this.getSystemService(Context.NOTIFICATION_SERVICE);
+//        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
+        Notification mBuilder = new Notification.Builder(getApplicationContext()).setSmallIcon(R.mipmap.ic_launcher).setContentTitle("PMS").setContentText(msg).build(); // 建立通知
+//        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this).setSmallIcon(R.mipmap.ic_launcher).setContentTitle("GCM Notification").setContentText(msg);
+        mNotificationManager.notify(NOTIFCATION_ID, mBuilder);
+//        final int notifyID = 1; // 通知的識別號碼
+//        final NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE); // 取得系統的通知服務
+//        final Notification notification = new Notification.Builder(getApplicationContext()).setSmallIcon(R.mipmap.ic_launcher).setContentTitle("內容標題").setContentText("內容文字").build(); // 建立通知
+//        notificationManager.notify(notifyID, notification); // 發送通知
+    }
+}
--- app/src/main/java/com/example/jude/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jude/myapplication/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.jude.myapplication;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -10,19 +14,20 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.HorizontalBarChart;
-import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.github.mikephil.charting.utils.ValueFormatter;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -31,18 +36,35 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class MainActivity extends ActionBarActivity {
     protected HorizontalBarChart mChart;
     private TableLayout table;
+
+    public static final String PROPERTY_REG_ID = "registration_id";
+    private static final String PROPERTY_APP_VERSION = "appVersion";
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST  = 9000;
+
+    String SENDER_ID = "413376427270";
+
+    static final String TAG = "GCMDemo";
+
+    GoogleCloudMessaging gcm;
+    AtomicInteger msgId = new AtomicInteger();
+    Context context;
+    String regid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mChart = (HorizontalBarChart) findViewById(R.id.chart1);
         table = (TableLayout)findViewById(R.id.table);
         mChart.setDrawValueAboveBar(false);
@@ -57,6 +79,21 @@ public class MainActivity extends ActionBarActivity {
         YAxis rightAxis = mChart.getAxisRight();
         rightAxis.setAxisMaxValue(100f);
         rightAxis.setValueFormatter(new PercentFormatter());
+
+        context = getApplicationContext();
+
+        if(checkPlayServices()){
+            //If this check succeeds, proceed with normal processing.
+            //Otherwise, prompt user to get valid Play Servies APK.
+            gcm = GoogleCloudMessaging.getInstance(this);
+            regid = getRegistrationId(context);
+            Log.i(TAG, regid);
+            if(regid.isEmpty()){
+                registerInBackground();
+            }
+        }else{
+            Log.i(TAG, "No valid Google Play Services APK found.");
+        }
 //        XAxis xAxis = mChart.getXAxis();
 //        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
 //        YAxis yaxis = new YAxis();
@@ -65,6 +102,101 @@ public class MainActivity extends ActionBarActivity {
 
         new getMain().execute();
 
+    }
+
+    private boolean checkPlayServices(){
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if(resultCode != ConnectionResult.SUCCESS){
+            if(GooglePlayServicesUtil.isUserRecoverableError(resultCode)){
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this, PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            }else{
+                Log.i(TAG, "This device is not supported.");
+            }
+            return false;
+        }
+        return true;
+    }
+
+    private String getRegistrationId(Context context){
+        final SharedPreferences prefs = getGCMPreferences(context);
+        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+        if(registrationId.isEmpty()){
+            Log.i(TAG, "Registration not found.");
+            return "";
+        }
+        // Check if app was updated; if so, it must clear the registration ID
+        // since the existing registration ID is not guaranteed to work with
+        // the new app version.
+        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
+        int currentVersion = getAppVersion(context);
+        if (registeredVersion != currentVersion) {
+            Log.i(TAG, "App version changed.");
+            return "";
+        }
+
+        return registrationId;
+    }
+
+    private static int getAppVersion(Context context){
+        try{
+            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e){
+            // should never happen
+            throw new RuntimeException("Could not get package name: "+ e);
+        }
+    }
+
+    private SharedPreferences getGCMPreferences(Context context){
+        // This sample app persists the registration ID in shared preferences, but
+        // how you store the registration ID in your app is up to you.
+        return getSharedPreferences(MainActivity.class.getSimpleName(), Context.MODE_PRIVATE);
+    }
+
+    private void registerInBackground(){
+        new AsyncTask(){
+            @Override
+            protected String doInBackground(Object[] params) {
+                String msg = "";
+                try{
+                    if(gcm == null){
+                        gcm = GoogleCloudMessaging.getInstance(context);
+                    }
+                    regid = gcm.register(SENDER_ID);
+                    msg = "Device registered, registration ID=" + regid;
+                    Log.i(TAG, regid);
+                    // You should send the registration ID to your server over HTTP,
+                    // so it can use GCM/HTTP or CCS to send messages to your app.
+                    // The request to your server should be authenticated if your app
+                    // is using accounts.
+                    sendRegistrationIdToBackend();
+
+                    // Persist the registration ID - no need to register again.
+                    storeRegistrationId(context, regid);
+                } catch (IOException ex){
+                    msg = "Error: " + ex.getMessage();
+                }
+                return msg;
+            }
+
+            protected void onPostExecute(String msg){
+                Log.i(TAG, msg);
+            }
+        }.execute(null,null,null);
+    }
+
+    private void sendRegistrationIdToBackend(){
+        // Your implementation here.
+    }
+
+    private void storeRegistrationId(Context context, String regId){
+        final SharedPreferences prefs = getGCMPreferences(context);
+        int appVersion = getAppVersion(context);
+        Log.i(TAG, "Saving regId on app version " + appVersion);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(PROPERTY_REG_ID, regId);
+        editor.putInt(PROPERTY_APP_VERSION, appVersion);
+        editor.commit();
     }
 
     public class PercentFormatter implements ValueFormatter {
